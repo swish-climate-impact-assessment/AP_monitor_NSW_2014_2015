03583e63f4fab809acf0396bd20a558cfefb9bd9
ivanhanigan <ivan.hanigan@gmail.com>
2016-01-04 22:06:26 +1100

    new code files

---
+++ code/clean.R
@@ -0,0 +1,5 @@
+# Project: AP_monitor_NSW_1996_2015
+# Author: Your Name
+# Maintainer: Who to complain to <yourfault@somewhere.net>
+
+# All the potentially messy data cleanup
---
+++ code/func.R
@@ -0,0 +1,9 @@
+# Project: AP_monitor_NSW_1996_2015
+# Author: Your Name
+# Maintainer: Who to complain to <yourfault@somewhere.net>
+
+# Functions for the project
+
+myFunc <- function(){
+
+}
